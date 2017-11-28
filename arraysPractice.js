4ff7933ae806aeba55bc52eee87accbe4071e11c
PowellBrand <powellbrandon@outlook.com>
2017-11-28 15:19:51 -0700

    more done

--- arraysPractice.js
+++ arraysPractice.js
@@ -57,7 +57,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
   //Code Here
 
-
+function evenFinder(nums){
+  var evens = [];
+  for(i =0; i< nums.length; i++){
+    if(nums[i] % 2 === 0){
+      evens.push(nums[i]);
+     }
+  }
+  return evens;
+}
 
 
   
